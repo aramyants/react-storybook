4e9394232bc1db5ab4540885e5469c85d7fc341c
aramyants <aramaramyants@gmail.com>
2025-08-05 11:04:45 +0400

    Control, viewport and actions addon

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -6,13 +6,15 @@ import { withThemeByClassName } from '@storybook/addon-themes';
 
 const preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    actions: { argTypesRegex: '^on.*' },
+
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+
     options: {
       // The `a` and `b` arguments in this function have a type of `import('storybook/internal/types').IndexEntry`. Remember that the function is executed in a JavaScript environment, so use JSDoc for IntelliSense to introspect it.
       storySort: (a, b) =>
@@ -20,8 +22,15 @@ const preview = {
           ? 0
           : a.id.localeCompare(b.id, undefined, { numeric: true }),
     },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    }
   },
-  
+
   decorators: [
     (Story) => (
       <ChakraProvider value={defaultSystem}>
--- src/components/ChakraButton/ChakraButton.stories.jsx
+++ src/components/ChakraButton/ChakraButton.stories.jsx
@@ -4,13 +4,29 @@ import {Button} from '@chakra-ui/react'
 export default {
   title: "Chakra/Button",
   component: Button,
+  argTypes: {
+    onClick: { action: "clicked" },
+    colorPalette: {
+      control: { type: "select" },
+      options: ["green", "red", "blue", "gray"],
+      description: "Color palette for the button",
+    },
+  },
 }
 
-export const Success = () => (
-  <Button colorPalette={"green"}> Success </Button>
-);
+const Template = args => <Button {...args} />;
 
-export const Danger = () => (
-  <Button colorPalette={"red"}> Danger </Button>
-);
+export const Success = Template.bind({});
+Success.args = {
+  colorPalette: "green",
+  children: "Success",
+};
+
+
+export const Danger = Template.bind({});
+Danger.args = {
+  colorPalette: "red",
+  children: "Danger",
+};
+
 
